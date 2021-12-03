c6afe639f03fcd568801f3cef4364b169969b7c1
Raul Comeros, Jr <redqweqwecode001@gmail.com>
2021-12-03 23:09:32 +0800

    feat: Add size to platform stats

--- Assets/ScriptableObjects/Platforms/PlatformStats.cs
+++ Assets/ScriptableObjects/Platforms/PlatformStats.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "PlatformStats_Type", menuName = "Platform/Platform Stats", order = 0)]
 public class PlatformStats : ScriptableObject
 {
+    [Header("Size")]
+    [SerializeField] private float x = 1f;
+    [SerializeField] private float y = 1f;
+    [SerializeField] private float z = 1f;
+
     [Header("Attribute")]
     [SerializeField] private float xMoveSpeed;
     [SerializeField] private float yMoveSpeed;
@@ -28,4 +33,7 @@ public class PlatformStats : ScriptableObject
     public Ease Ease { get => ease; }
     public float XOffset { get => xOffset; }
     public float YOffset { get => yOffset; }
+    public float X { get => x; }
+    public float Y { get => y; }
+    public float Z { get => z; }
 }
