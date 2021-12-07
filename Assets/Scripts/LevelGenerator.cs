198a989f982fcfad78b31545f84956e69c77019a
Raul Comeros, Jr <redqweqwecode001@gmail.com>
2021-12-07 22:09:02 +0800

    refactor: Rename WorldGenerator.cs to LevelGenerator

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WorldGenerator : MonoBehaviour
+public class LevelGenerator : Singleton<LevelGenerator>
 {
     public Transform playerTransform;
     public Transform triggerPoint;
@@ -16,13 +16,30 @@ public class WorldGenerator : MonoBehaviour
 
     [SerializeField] private LevelGenData levelGenData;
 
+    [SerializeField] private PlatformStats stillPlatfromStats;
+    [SerializeField] private PlatformStats movingPlatfromStats;
+    private int platformDirection = 1;
+
     private void Update() 
     {
-
         if (playerTransform.position.y > triggerPoint.position.y)
         {
             Generate();
             triggerPoint.position = new Vector3(triggerPoint.position.x, triggerPoint.position.y + triggerMoveDistance, transform.position.z);
+        }
+    }
+
+    internal PlatformStats GetPlatformStats(PlatformType type)
+    {
+        switch (type)
+        {
+            case PlatformType.Still:
+                return stillPlatfromStats;
+            case PlatformType.Moving:
+                return movingPlatfromStats;
+            default:
+                Debug.LogError("Invalid PlatformType.");
+                return null;
         }
     }
 
@@ -36,9 +53,18 @@ public class WorldGenerator : MonoBehaviour
             newPosition.x = Random.Range(xOffset, -xOffset);
 
             GameObject p = platforms[Random.Range(0, platforms.Length)];
-
+            var ps = p.GetComponent<Platform>();
+            if (ps.Type == PlatformType.Moving)
+            {
+                // p.GetComponent<Platform>().InitialDirection = platformDirection;
+                ps.initialDirection = platformDirection;
+                Debug.Log("PD: " + platformDirection);
+                platformDirection *= -1;
+            }
+            
             Instantiate(p, newPosition, Quaternion.identity);
             transform.position = newPosition;
+
         }
     }
 }
