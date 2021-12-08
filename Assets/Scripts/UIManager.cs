79249adcdb01e33637ee705ac981a095080656a7
Raul Comeros, Jr <redqweqwecode001@gmail.com>
2021-12-08 20:06:51 +0800

    feat(uimanager): Add UIManager script

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UIManager : MonoBehaviour
+{
+    // refactor to scoremanager
+    [SerializeField] private TextMeshProUGUI txtScore;
+
+
+    private void Update()
+    {
+        txtScore.text = ((int) ScoreManager.Instance.CurrentScore).ToString();
+    }
+}
