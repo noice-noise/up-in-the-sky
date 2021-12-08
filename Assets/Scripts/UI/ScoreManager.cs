58bba3a6b961837c0521f117b4dea41f7390d403
Raul Comeros, Jr <redqweqwecode001@gmail.com>
2021-12-08 20:07:44 +0800

    feat: Add ScoreManager script

---
+++ Assets/Scripts/UI/ScoreManager.cs
@@ -0,0 +1,26 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class ScoreManager : Singleton<ScoreManager>
+{
+
+    private float currentScore;
+
+    public float CurrentScore { get => currentScore; }
+
+    [SerializeField] Transform playerTransform;
+
+    private void Update() {
+        var playerPos =  playerTransform.position.y;
+        if ( playerPos > currentScore)
+        {
+            float addScore = playerPos - currentScore;
+            AddScore(addScore);
+        }
+    }
+
+    public void AddScore(float score)
+    {
+        currentScore += score;
+    }
+}
