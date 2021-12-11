d33a73a5bb928d8f765b4c8e0dd0b6215e304492
Raul Comeros, Jr <redqweqwecode001@gmail.com>
2021-12-11 20:54:51 +0800

    chore: Add DifficultyData.cs

--- Assets/Project/Runtime/Scripts/ScriptableObjects/DifficultyData.cs
+++ Assets/Project/Runtime/Scripts/ScriptableObjects/DifficultyData.cs
@@ -1,7 +1,8 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 [CreateAssetMenu(fileName = "DifficultyData", menuName = "UpInTheSky/DifficultyData", order = 0)]
 public class DifficultyData : ScriptableObject
 {
-        
+    public List<LevelDifficulty> levels;
 }
