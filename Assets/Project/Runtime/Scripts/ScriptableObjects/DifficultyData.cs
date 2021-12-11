7f4a171bf695c8a5ce3456d0791eb8abecb5acb2
Raul Comeros, Jr <redqweqwecode001@gmail.com>
2021-12-11 20:51:27 +0800

    chore: Move scriptable object scripts to Script folder

---
+++ Assets/Project/Runtime/Scripts/ScriptableObjects/DifficultyData.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "DifficultyData", menuName = "UpInTheSky/DifficultyData", order = 0)]
+public class DifficultyData : ScriptableObject
+{
+        
+}
