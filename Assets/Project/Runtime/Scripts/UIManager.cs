7f2ed029b444d963e1c49b6f8495e5ee517c0bbd
Raul Comeros, Jr <redqweqwecode001@gmail.com>
2021-12-11 11:13:52 +0800

    feat: Add in game statistics overlay

--- Assets/Project/Runtime/Scripts/UIManager.cs
+++ Assets/Project/Runtime/Scripts/UIManager.cs
@@ -2,16 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using System;
 
 public class UIManager : MonoBehaviour
 {
     // refactor to scoremanager
     [SerializeField] private TextMeshProUGUI txtScore;
-
+    [SerializeField] private TextMeshProUGUI txtDebug;
 
     private void Update()
     {
         txtScore.text = ((int) ScoreManager.Instance.CurrentScore).ToString();
-        // Application.logMessageReceived
+        DisplayDebugOverlay();
+    }
+
+    private void DisplayDebugOverlay()
+    {
+        txtDebug.text 
+            = "Current Position: " + GameManager.Instance.player.position
+            + ""
+            ;
     }
 }
